eb742921506c6407eb1543f72249597e0d651d0b
stelselim <selim.ustel1998@gmail.com>
2021-05-09 11:50:52 +0300

    first

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import AppNav from './src/AppNav';
+const App = () => {
+
+  return (
+    <AppNav />
+  );
+};
+export default App;
---
+++ src/AppNav.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { SafeAreaView, Text, View } from 'react-native';
+
+const AppNav = () => {
+    return (
+        <SafeAreaView>
+            <View style={{ alignSelf: "center" }}>
+                <Text>
+                    Hello React Native
+        </Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+export default AppNav;
